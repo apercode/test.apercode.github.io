6bd5a23824cd054deb2bf426a04f072e762b47f1
apercode <apercode@users.noreply.github.com>
2020-04-08 16:36:43 +0300

    subcopy
    new number

---
+++ js/js.js
@@ -0,0 +1,85 @@
+
+
+document.getElementById('but1').onclick = function but1 (){
+    document.getElementById('inf1').style.display = 'block';
+    document.getElementById('inf2').style.display = 'none'; 
+    document.getElementById('inf3').style.display = 'none'; 
+    document.getElementById('inf4').style.display = 'none'; 
+    document.getElementById('inf5').style.display = 'none'; 
+    document.getElementById('inf6').style.display = 'none'; 
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none'; 
+    }
+document.getElementById('but2').onclick = function but2 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'block';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none';
+    }
+document.getElementById('but3').onclick = function but3 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'block';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none';
+    }
+document.getElementById('but4').onclick = function but4 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'block';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none';
+}
+document.getElementById('but5').onclick = function but5 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'block';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none';
+}
+document.getElementById('but6').onclick = function but6 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'block';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'none';
+}
+document.getElementById('but7').onclick = function but7 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'block';
+    document.getElementById('inf8').style.display = 'none';
+}
+document.getElementById('but8').onclick = function but8 (){
+    document.getElementById('inf1').style.display = 'none';
+    document.getElementById('inf2').style.display = 'none';
+    document.getElementById('inf3').style.display = 'none';
+    document.getElementById('inf4').style.display = 'none';
+    document.getElementById('inf5').style.display = 'none';
+    document.getElementById('inf6').style.display = 'none';
+    document.getElementById('inf7').style.display = 'none';
+    document.getElementById('inf8').style.display = 'block';
+}
+document.getElementById('contactButton').addEventListener('click', function() {
+    insert_event_code_here;
+    }, false);
